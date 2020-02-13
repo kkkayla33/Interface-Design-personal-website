c5f876326f7b84c05646f372ce0e855e9bae81da
kkkayla33 <59672356+kkkayla33@users.noreply.github.com>
2020-02-13 18:36:00 -0500

    Create education.js

---
+++ education.js
@@ -0,0 +1,180 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Hello!</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <!-- import the webpage's stylesheet -->
+    <link rel="stylesheet" href="/style.css">
+    
+    <!-- import the webpage's javascript file -->
+    <script src="/edujs.js" defer></script>
+  </head>  
+  
+  <body>
+    
+    <header>
+      <nav>
+        <a href="/index.html">WELCOME </a> |
+        <a href="/education.html">EDUCATION</a> |
+        <a href="/photo.html">PHOTOGRAPH</a> |
+        <a href="/movie.html">MOVIE</a> | 
+        <a href="/contact.html">CONTACT </a>
+      </nav>
+    </header>
+    
+    <br>
+    <br>
+    <br>
+    
+    <h1> 
+       Education Experience
+    </h1>
+    
+        <br>
+        <br>
+        <br>
+        <br>
+ 
+    <section>
+      
+      <p class="listTitle">
+        Institute: University of Pittsburgh 
+        <br>
+        <a href="https://www.pitt.edu"> Go to Pitt Home</a>
+      </p>
+            <p id="list">
+               <ul >
+                  <li>   Career: Graduate
+                      <ul>
+                        <li> Major: Information Science</li>
+                        <li> Track: Human Centered Computing</li>
+                      </ul>   
+                  </li>
+                  <li>  Class Schedual 
+                      <ul>
+                        <li><a href="#" id="spring">Weekly Schedual </a></li>
+<!--                         <li><a href="#" id="fall"> Fall Term 2019-2020 </a></li> -->
+                      </ul>
+                  </li>
+
+                </ul>
+            </p>
+    </section>
+  
+      <article>
+          <div id="springSchedual" style="opacity:0">
+              <table style="width:100%" >
+                    <tr>
+                      <th class="tableTitle" >Schedule</th>
+                      <th class="tableTitle" >Monday</th> 
+                      <th class="tableTitle" >Tuesday</th>
+                      <th class="tableTitle" >Wednesday</th>
+                      <th class="tableTitle" >Thursday</th> 
+                      <th class="tableTitle" >Friday</th>
+                    </tr>
+                    <tr>
+                      <td class="tableTime" >9:00 am</td>
+                      <td class="studyGroup" >Will Study Group 10:00-11:00</td>
+                      <td></td>
+                      <td class="studyGroup">Will Study Group 10:00-11:00</td>
+                      <td class="course">Web Technology</td>
+                      <td></td>
+                    </tr>
+                    <tr>
+                      <td class="tableTime" >12:00 pm</td>
+                      <td class="course">Interactive System Design</td>
+                      <td class="studyGroup">AD Study Group 1:30-3:00</td>
+                      <td class="course">Social Computing</td>
+                      <td class="course">9:00-11:50</td>
+                      <td></td>
+                    </tr>
+                    <tr>
+                      <td class="tableTime" >3:00 pm</td>
+                      <td class="course">12:00-2:50</td>
+                      <td></td>
+                      <td class="course">12:00-2:50</td>
+                      <td></td>
+                      <td></td>
+                    </tr>
+                    <tr>
+                      <td class="tableTime" >6:00 pm</td>
+                      <td class="course">Algorithm Design</td>
+                      <td></td>
+                      <td class="studyGroup">ISD Study Group </td>
+                      <td></td>
+                      <td></td>
+                    </tr>
+                    <tr>
+                      <td class="tableTime" >9:00 pm</td>
+                      <td class="course">6:00-8:50</td>
+                      <td></td>
+                      <td class="studyGroup">6:00-9:00</td>
+                      <td></td>
+                      <td></td>
+                    </tr>
+
+              </table>
+             </div>
+<!--       
+             <div id="fallSchedual" style="opacity:1">
+               <table style="width:100%" >
+                      <tr>
+                        <th class="tableTitle" >Schedule</th>
+                        <th class="tableTitle" >Monday</th> 
+                        <th class="tableTitle" >Tuesday</th>
+                        <th class="tableTitle" >Wednesday</th>
+                        <th class="tableTitle" >Thursday</th> 
+                        <th class="tableTitle" >Friday</th>
+                      </tr>
+                      <tr>
+                        <td class="tableTime" >9:00 am</td>
+                        <td class="studyGroup" >Will Study Group 10:00-11:00</td>
+                        <td></td>
+                        <td class="studyGroup">Will Study Group 10:00-11:00</td>
+                        <td class="course">Web Technology</td>
+                        <td></td>
+                      </tr>
+                      <tr>
+                        <td class="tableTime" >12:00 pm</td>
+                        <td class="course">Interactive System Design</td>
+                        <td class="studyGroup">AD Study Group 1:30-3:00</td>
+                        <td class="course">Social Computing</td>
+                        <td class="course">9:00-11:50</td>
+                        <td></td>
+                      </tr>
+                      <tr>
+                        <td class="tableTime" >3:00 pm</td>
+                        <td class="course">12:00-2:50</td>
+                        <td></td>
+                        <td class="course">12:00-2:50</td>
+                        <td></td>
+                        <td></td>
+                      </tr>
+                      <tr>
+                        <td class="tableTime" >6:00 pm</td>
+                        <td class="course">Algorithm Design</td>
+                        <td></td>
+                        <td class="studyGroup">ISD Study Group </td>
+                        <td></td>
+                        <td></td>
+                      </tr>
+                      <tr>
+                      <td class="tableTime" >9:00 pm</td>
+                      <td class="course">6:00-8:50</td>
+                      <td></td>
+                      <td class="studyGroup">6:00-9:00</td>
+                      <td></td>
+                      <td></td>
+                    </tr>
+
+              </table>
+              </div> -->
+      
+        </article>
+        
+
+  </body>
+</html>
