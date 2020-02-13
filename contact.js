da401c9d3f9bdb0f3b9f6848158d1a5f69bd4762
kkkayla33 <59672356+kkkayla33@users.noreply.github.com>
2020-02-13 18:35:37 -0500

    Create contact.js

---
+++ contact.js
@@ -0,0 +1,8 @@
+/*                         contact                          */
+
+
+function submit(){
+  alert("Successfully submit! I would reply you soon :D");
+}
+
+document.querySelector('#submit').addEventListener('click', submit);
